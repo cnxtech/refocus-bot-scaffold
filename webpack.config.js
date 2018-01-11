1bf08bf538bd4c06aee7ca31b13cce4a8e35c525
ksmuga <ksmuga@salesforce.com>
2018-01-11 13:57:32 +0000

    webpack perf improvements

--- webpack.config.js
+++ webpack.config.js
@@ -18,10 +18,25 @@ var config = {
 
   module: {
     rules: [
-      {test: /\.(js|jsx)$/, use: 'babel-loader?compact=true'}, //code transformer (if file is .js)
-      {test: /\.css$/, use: ['style-loader', 'css-loader']},
-      {test: /\.handlebars$/, loader: "handlebars-loader"},
-      {test: /.(png|jpg|jpeg|gif|svg|woff|woff2|ttf|eot)$/, use: "url-loader?limit=100000"},
+      {
+        test: /\.(js|jsx)$/,
+        use: 'babel-loader?compact=true',
+        include: [path.resolve(__dirname, 'lib'), path.resolve(__dirname, 'web')]
+      }, //code transformer (if file is .js)
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.handlebars$/,
+        loader: 'handlebars-loader',
+        include: path.resolve(__dirname, 'web')
+      },
+      {
+        test: /.(png|jpg|jpeg|gif|svg|woff|woff2|ttf|eot)$/,
+        use: 'url-loader?limit=100000',
+        include: path.resolve(__dirname, 'web')
+      },
     ]
   },
 
